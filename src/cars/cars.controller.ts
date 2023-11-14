b20bc3d3e5d5121509dae19362ebf2ac9a7e1b91
Luis Miguel Del Gallego <luis.delgallego@licify.co>
2023-11-14 09:04:00 -0500

    🌟ADD: Add validations pipe

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -7,10 +7,14 @@ import {
   ParseUUIDPipe,
   Patch,
   Post,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
 import { CarsService } from './cars.service';
+import { CreateCarDto } from './dto/create-car.dto';
 
 @Controller('cars')
+@UsePipes(ValidationPipe)
 export class CarsController {
   constructor(private readonly carsService: CarsService) {}
 
@@ -25,7 +29,7 @@ export class CarsController {
   }
 
   @Post()
-  createCar(@Body() body: any) {
+  createCar(@Body() body: CreateCarDto) {
     return {
       ok: true,
       body,
